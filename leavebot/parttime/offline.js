768bd05f5b1c8f0b3e385053a203e3a40b862b3e
Tom Mortimer <tom.mortimer@citizensadvice.org.uk>
2019-06-04 10:29:52 +0100

    Deploy new Leave Bot
    This is the new CAB Leave Calculator (JS/Canvas)

---
+++ leavebot/parttime/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1559576600,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/header-sheet0.png",
+		"images/flow1bg.png",
+		"images/flow2bg.png",
+		"images/fulltimebtn-sheet0.png",
+		"images/ftstartdateblanker-sheet0.png",
+		"images/datevalidatestart-sheet0.png",
+		"images/datevalidateend-sheet0.png",
+		"images/homebg.png",
+		"images/homeflowl.png",
+		"images/homeflowr.png",
+		"images/flow1hover1-sheet0.png",
+		"images/flow2hover1-sheet0.png",
+		"images/hrbutton-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
