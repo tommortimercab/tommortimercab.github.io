4addb464041fa5d8079271727d4bef6781b0e8c3
Tom Mortimer <tom.mortimer@citizensadvice.org.uk>
2019-06-12 11:34:04 +0100

    Initial commit
    Needs work

---
+++ meeting-rooms/script.js
@@ -0,0 +1,13 @@
+/*Air temp START*/
+/*A standard AJAX call that fetches air temperature in celsius.*/
+$.ajax({
+    url: "http://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=c6a2103599b372904ac22fcd5047443a",
+    dataType: 'json',
+    success: function(results){
+        /*Rounds the temperature so decimal points are not displayed*/
+        var airtemp = Math.round(results.main.temp);
+        $('#air').html(airtemp);
+    }
+});
+
+/*Air temp END*/
