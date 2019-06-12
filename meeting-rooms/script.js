0415e1b26cdaf6f71984a9d058cfe232201c20f7
Tom Mortimer <tom.mortimer@citizensadvice.org.uk>
2019-06-12 11:37:15 +0100

    owm key

--- meeting-rooms/script.js
+++ meeting-rooms/script.js
@@ -1,7 +1,7 @@
 /*Air temp START*/
 /*A standard AJAX call that fetches air temperature in celsius.*/
 $.ajax({
-    url: "http://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=c6a2103599b372904ac22fcd5047443a",
+    url: "https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=c6a2103599b372904ac22fcd5047443a",
     dataType: 'json',
     success: function(results){
         /*Rounds the temperature so decimal points are not displayed*/
